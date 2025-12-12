78ff8fdec2f72afe982d9ad1610ef393a7692b7e
lijingwen <lijingwen_a@aspirecn.com>
2025-12-12 17:02:07 +0800

    feat:更新阴阳胶囊配置

--- src/components/common/YinYangCapsule.vue
+++ src/components/common/YinYangCapsule.vue
@@ -1,18 +1,10 @@
 <template>
   <div class="pagination-capsule">
-    <div 
-      class="capsule-segment segment-left"
-      :class="{ disabled: disabledLeft }"
-      @click="emit('click-left')"
-    >
+    <div class="capsule-segment segment-left" :class="{ disabled: disabledLeft }" @click="emit('click-left')">
       &lt;
     </div>
-    
-    <div 
-      class="capsule-segment segment-right"
-      :class="{ disabled: disabledRight }"
-      @click="emit('click-right')"
-    >
+
+    <div class="capsule-segment segment-right" :class="{ disabled: disabledRight }" @click="emit('click-right')">
       &gt;
     </div>
   </div>
@@ -37,34 +29,45 @@ const emit = defineEmits<{
 
 <style scoped>
 .pagination-capsule {
+  --capsule-bg-left: #474747;
+  --capsule-text-left: #ffffff;
+  --capsule-border-left: #474747;
+  --capsule-bg-right: #e5e5ea;
+  --capsule-text-right: #1c1b1f;
+  --capsule-border-right: #e5e5ea;
+
   display: flex;
-  gap: 2px;
+  gap: 3px;
   cursor: pointer;
+  /* 禁止用户变蓝拖拽选中 */
   user-select: none;
 }
 
 .capsule-segment {
-  width: 24px;
-  height: 24px;
+  width: 20px;
+  height: 20px;
   display: flex;
   align-items: center;
   justify-content: center;
+
   font-family: system-ui, sans-serif;
   font-weight: 800;
-  font-size: 14px;
+  font-size: 12px;
   line-height: 1;
   transition: all 0.2s ease;
+  /* 使得宽度为20 而不是20+border */
   box-sizing: border-box;
 }
 
 .capsule-segment:hover {
-  filter: brightness(1.2);
-  transform: translateY(-1px);
+  opacity: 0.8;
+  /* filter: brightness(1.2);
+  transform: translateY(-1px); */
 }
 
-.capsule-segment:active {
+/* .capsule-segment:active {
   transform: translateY(1px);
-}
+} */
 
 .capsule-segment.disabled {
   opacity: 0.3;
@@ -77,13 +80,13 @@ const emit = defineEmits<{
   background: var(--capsule-bg-left);
   color: var(--capsule-text-left);
   border: 1px solid var(--capsule-border-left);
-  border-radius: 12px 4px 4px 12px;
+  border-radius: 10px 2px 2px 10px;
 }
 
 .segment-right {
   background: var(--capsule-bg-right);
   color: var(--capsule-text-right);
   border: 1px solid var(--capsule-border-right);
-  border-radius: 4px 12px 12px 4px;
+  border-radius: 2px 10px 10px 2px;
 }
 </style>
